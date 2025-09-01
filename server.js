8f4420fa80e87dd153c295be46c08c06cd03db20
NaphatJM <NaphatJM.work@gmail.com>
2025-09-01 16:59:16 +0700

    fix

--- server.js
+++ server.js
@@ -28,6 +28,8 @@ app.use((req, res, next) => {
 
 // Code Smell #1
 // TODO: this is first code smell
+// Code Smell #2
+// TODO: this is second code smell
 app.get("/", (req, res) => {
     console.log(">>> Checkout page visited");
     res.send("Hello World!");
